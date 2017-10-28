38b7b6fcf9e0fd248fa532c5c352ecb7336e23ea
Samuel Delagrange <1000416@sacs.k12.in.us>
2017-10-28 01:10:07 -0400

    Trying to make calendar look nice

---
+++ calendar/calendar.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"><html><head><meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Calendar ~ HHS DECA</title>
+<script>gcal$perf$serverTime=15;gcal$perf$headStartTime=new Date().getTime()</script>
+<style type="text/css">body{margin:0;padding:0;overflow-y:hidden;}
+        html{overflow-y:hidden;}</style>
+<link type="text/css" rel="stylesheet" href="https://calendar.google.com/calendar/static/34d859f58f18491bda8e1aa9db776d6dembedcompiled_fastui.css">
+
+
+<script type="text/javascript">
+  function _DumpException(e) { throw e; }
+  </script>
+<script type="text/javascript" src="https://calendar.google.com/calendar/_/scs/calendar-static/_/js/k=calendar.embed.en.OW8ZcxRrAkk.O/m=embed/rt=j/d=1/rs=ANwU0p4O9w6q6bDjn018w-XowxLGk0hjBg"></script>
+<script>function _onload() {window._init({"features":[1,1,1,1,0,1,1,1,0,1,1,0,1,1,0,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,1,1,0,1,1,0,1,0,0,0,0,1,1,0,0,1,1,1,1,0,1,1,1,1,1,0,0,1,1,0,0,0,0,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,0,1,1,1,0,1,1,1,1,1,1,1,0,0,0,1,0,1,0,1,0,1,1,0,1,1,1,0,1,0,0,1,0,0,0,0,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,0,1,1,0,1,1,1,0],"loggedin":true,"cids":{},"view":"month","weekstart":0,"format24hour":false,"dateFieldOrder":0,"preloadStart":"20170930","preloadEnd":"20171107","container":"container","baseUrl":"/","imagePath":"https://calendar.google.com/googlecalendar/images/","showNavigation":true,"showDateMarker":true,"showTabs":true,"showCalendarMenu":false,"showCurrentTime":false,"showTz":false,"showPrintButton":false,"showElementsLogo":false,"collapseAllday":false,"showSubscribeButton":false,"embedStyle":"WyJhdDplbWI6c3QiXQo\u003d","proxyUrl":"https://clients6.google.com","calendarApiVersion":"v3","developerKey":"AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"});}</script>
+<script type="text/javascript">
+      var pageLoaded_ = false;
+      var clientLibraryLoaded_ = false;
+
+      function clientLibraryLoaded() {
+        clientLibraryLoaded_ = true;
+        if (pageLoaded_) _onload();
+      }
+
+      function pageLoaded() {
+        pageLoaded_ = true;
+        if (clientLibraryLoaded_) _onload();
+      }
+    </script>
+<script type="text/javascript" src="https://apis.google.com/js/client.js?onload=clientLibraryLoaded"></script>
+<style type="text/css">
+
+  </style></head>
+<body onload="pageLoaded()" style="background-color:#fff">
+
+
+<noscript><p></p>
+Your browser does not appear to support JavaScript, but this page needs to use JavaScript to display correctly.
+You can visit the HTML-only version of this page at:
+<a href="https://calendar.google.com/calendar/b/1/htmlembed?showTitle=0&amp;amp;showPrint=0&amp;amp;showTabs=0&amp;amp;showCalendars=0&amp;amp;showTz=0&amp;amp;height=600&amp;amp;wkst=1&amp;amp;bgcolor=%23FFFFFF&amp;amp;src=1hms077l0t6ets4n2ujuor2r9s%40group.calendar.google.com&amp;amp;color=%232F6309&amp;amp;ctz=America%2FNew_Yor">https://calendar.google.com/calendar/b/1/htmlembed?showTitle=0&amp;amp;showPrint=0&amp;amp;showTabs=0&amp;amp;showCalendars=0&amp;amp;showTz=0&amp;amp;height=600&amp;amp;wkst=1&amp;amp;bgcolor=%23FFFFFF&amp;amp;src=1hms077l0t6ets4n2ujuor2r9s%40group.calendar.google.com&amp;amp;color=%232F6309&amp;amp;ctz=America%2FNew_Yor</a></noscript>
+<div id="container" style="width:100%" class="locale-en "></div></body></html>
